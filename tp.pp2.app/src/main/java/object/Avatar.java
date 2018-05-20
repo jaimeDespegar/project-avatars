e7b00e222fa0273a5c3d1c5955c1fa5b87babdbb
fjgalvan <fjgalvanx87@gmail.com>
2018-05-20 05:09:02 -0300

    Avatar

--- tp.pp2.app/src/main/java/object/Avatar.java
+++ tp.pp2.app/src/main/java/object/Avatar.java
@@ -1,5 +1,44 @@
 package object;
 
+import java.awt.Point;
+import java.util.HashMap;
+
 public class Avatar {
+	
+	protected Point coordinate;
+	protected Point size;
+	protected boolean escudo;
+	
+	public Avatar(Point coordinate, Point size, boolean escudo) {
+		this.coordinate = coordinate;
+		this.size = size;	
+		this.escudo= escudo;
+	}
+	
+	public Point getCoordinate() {
+		return coordinate;
+	}
+	public void setCoordinate(Point coordinate) {
+		this.coordinate = coordinate;
+	}
+	public Point getSize() {
+		return size;
+	}
+	public void setSize(Point size) {
+		this.size = size;
+	}
+	public boolean isEscudo() {
+		return escudo;
+	}
+
+	public void setEscudo(boolean escudo) {
+		this.escudo = escudo;
+	}
+
+	public Point assignFreePosition(HashMap mapa){ //el mapa contiene(Point posición, Object objeto)
+		Point freePosition= new Point(0,0); //valido inicial
+		//CONTROLO UNA POSICIÓN LIBRE DEL MAPA
+		return freePosition;
+	}
 
 }
