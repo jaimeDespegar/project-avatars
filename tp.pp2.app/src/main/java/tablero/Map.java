e3cea0e9cb3d8489db981fe22bb3a8d6768e1642
fjgalvan <fjgalvanx87@gmail.com>
2018-05-22 18:26:23 -0300

    No puedo agregar objetos al map ni saber que objetos hay en cada casillero !

--- tp.pp2.app/src/main/java/tablero/Map.java
+++ tp.pp2.app/src/main/java/tablero/Map.java
@@ -14,4 +14,8 @@ public class Map {
 	public void printMap() {
 		this.board.printBoard();
 	}
+	
+	/*public void printMapWhithNameObject(){ //Quiero Imprimir el tablero con el nobre de cada Objeto ó si es null el casillero
+		this.board.printBoardObjectName();
+	}*/
 }
