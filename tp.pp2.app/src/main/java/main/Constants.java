933b0129476991efcadec1668c6fda946e5bf978
Lautaro <lautarodebenedetti@gmail.com>
2018-05-27 11:24:04 -0300

    changes

---
+++ tp.pp2.app/src/main/java/main/Constants.java
@@ -0,0 +1,6 @@
+package main;
+
+public class Constants {
+	public static final String ROUTE_PROPERTIES = "src/main/java/properties/archivoMap.properties";
+	
+}
--- tp.pp2.app/src/test/java/criteriaOfAcceptance/UserStory_02.java
+++ tp.pp2.app/src/test/java/criteriaOfAcceptance/UserStory_02.java
@@ -7,51 +7,47 @@ import static org.junit.Assert.assertNull;
 
 import java.awt.Point;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import enums.TypeOfStructure;
-import tablero.Box;
 import tablero.Builder;
 import tablero.Map;
 import tablero.ObjectGraphic;
 import tablero.Rectangle;
 import tablero.Structure;
 
-/**
- * 
- * Se crea un mapa vacío de tamaño (largo = 2, ancho = 2). Se crea un Object en
- * la posición (1,1).
- * 
- * _Si creo un objeto en la posición (0,1), deberá poder ya que estaba vacío.
- * _Si Se intenta crear otro Object en la posición (1,1), no se crea, porque la
- * posición (1,1) ya está ocupada.
- *
- * 
- */
+/**Se crea un mapa vacío de tamaño (largo = 2, ancho = 2). Se crea un Object en
+ * la posición (1,1). */
 public class UserStory_02 {
-	private Rectangle rectangle = new Rectangle(new Point(1, 1), new Point(1, 1),
-			new Structure(null, TypeOfStructure.ACERO));
-	private Map map = new Builder(new Point(2, 2)).withStructureRectangle(rectangle).build();
+	private Rectangle rectangle;
+	private Map map;
+	
+	@Before
+	public void init() {
+		rectangle = new Rectangle(new Point(1, 1), new Point(1, 1),new Structure(TypeOfStructure.ACERO));
+		map = new Builder(new Point(2, 2)).withStructureRectangle(rectangle).build();
+	}
 	
 	/**En la posicion (0,1), debe estar vacia*/
 	@Test
 	public void testEmptyBox() {
-		assertNull(map.getBoard().getBoard()[0][1]);
+		assertNull(map.getBoard().getBoxes()[0][1]);
 	}
 
 	/** Si creo un objeto en la posición (0,1), deberá poder, ya que estaba vacío. */
 	@Test
 	public void testCreateObject() {
-		map.getBoard().addObjectGraphic(new Point (0,1), new ObjectGraphic (new Point (0,1)));
-		assertNotNull(map.getBoard().getBoard()[0][1].getObjectGraphic());
+		assertNull(map.getBoard().getBoxes()[0][1]);
+		map.getBoard().addBox(new Point (0,1), new ObjectGraphic ());
+		assertNotNull(map.getBoard().getBoxes()[0][1].getObjectGraphic());
 	}
 
-	/**
-	 * Si Se intenta crear otro Object en la posición (1,1), no se crea, porque la
-	 * posición (1,1) ya está ocupada.
-	 */
+	/**Si Se intenta crear otro Object en la posición (1,1), no se crea, porque la
+	 * posición (1,1) ya está ocupada. */
 	@Test
 	public void testFailCreateObject() {
-		assertNull(map.getBoard().getBoard()[0][1].getObjectGraphic());
+		map.getBoard().addBox(new Point (0,1), new ObjectGraphic ());
+		assertNull(map.getBoard().getBoxes()[0][1].getObjectGraphic());
 	}
 }
