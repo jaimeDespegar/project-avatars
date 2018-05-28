097c3b37282cd66d95f8d0cd9a6e65301b94a58e
Lautaro <lautarodebenedetti@gmail.com>
2018-05-28 18:47:15 -0300

    map properties

--- tp.pp2.app/src/main/java/classProperties/MapProperties.java
+++ tp.pp2.app/src/main/java/classProperties/MapProperties.java
@@ -4,6 +4,8 @@ import java.awt.Point;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import enums.TypeOfStructure;
@@ -28,9 +30,19 @@ public class MapProperties {
 				"rectangle1typeOfStructure");
 		Rectangle rectangle2 = loadRectangle("rectangle2posX", "rectangle2posY", "rectangle2ancho", "rectangle2alto",
 				"rectangle2typeOfStructure");
-		Line line1 = null;
-		Line line2 = null;
+		Line line1 = loadLine("line1posX1", "line1posY1", "line1posX2", "line1posY2", "line1typeOfStructure");
+		Line line2 = loadLine("line2posX1", "line2posY1", "line2posX2", "line2posY2", "line2typeOfStructure");
 		this.electionMap = new ElectionMap(mapSize, rectangle1, rectangle2, line1, line2);
+	}
+
+	private Line loadLine(String linePosX1, String linePosY1, String linePosX2, String linePosY2, String lineTypeOfStructure) {
+		Point point1 = new Point(Integer.parseInt(properties.getProperty(linePosX1)), Integer.parseInt(properties.getProperty(linePosY1)));
+		Point point2 = new Point(Integer.parseInt(properties.getProperty(linePosX2)), Integer.parseInt(properties.getProperty(linePosY2)));
+		Structure structure = loadStructure(lineTypeOfStructure);
+		List<Point> list = new ArrayList<Point>();
+		list.add(point1);
+		list.add(point2);
+		return new Line(list, structure);
 	}
 
 	private Rectangle loadRectangle(String rectangleposX, String rectangleposY, String rectangleAlto,
@@ -40,12 +52,12 @@ public class MapProperties {
 		Integer rectangle1ancho = Integer.parseInt(properties.getProperty(rectangleAlto));
 		Integer rectangle1alto = Integer.parseInt(properties.getProperty(rectangleAncho));
 		String rectangle1typeOfStructure = properties.getProperty(rectangletypeOfStructure);
-		Structure structureRectangle = loadStructureRectangle(rectangle1typeOfStructure);
+		Structure structureRectangle = loadStructure(rectangle1typeOfStructure);
 		return new Rectangle(new Point(rectangle1posX, rectangle1posY), new Point(rectangle1ancho, rectangle1alto),
 				structureRectangle);
 	}
 
-	private Structure loadStructureRectangle(String typeOfStructure) {
+	private Structure loadStructure(String typeOfStructure) {
 		Structure structure = new Structure(TypeOfStructure.ACERO);
 		return structure;
 	}
