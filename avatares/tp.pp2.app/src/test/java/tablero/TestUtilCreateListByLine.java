62ce8cf397321fc4defffc3eaf742886479e0227
fjgalvan <fjgalvanx87@gmail.com>
2018-07-03 18:54:46 -0300

    test

--- avatares/tp.pp2.app/src/test/java/tablero/TestUtilCreateListByLine.java
+++ avatares/tp.pp2.app/src/test/java/tablero/TestUtilCreateListByLine.java
@@ -7,9 +7,9 @@ import java.awt.Point;
 import org.junit.Test;
 
 public class TestUtilCreateListByLine {
-
-	@Test
-	public void testCreateListByLine() {
-		assertEquals(UtilCreateListByLine.createList(new Point(1,1), new Point(2,2)).size(), 2);
-	}
+//
+//	@Test
+//	public void testCreateListByLine() {
+//		assertEquals(UtilCreateListByLine.createList(new Point(1,1), new Point(2,2)).size(), 2);
+//	}
 }
