0bc2d710549b1bba6abcfea06ceb7b1de9039a6e
fjgalvan <fjgalvanx87@gmail.com>
2018-07-09 11:46:50 -0300

    tests

--- avatares/tp.pp2.app/src/test/java/classProperties/ElectionKeyAvatarWithShieldTest.java
+++ avatares/tp.pp2.app/src/test/java/classProperties/ElectionKeyAvatarWithShieldTest.java
@@ -1,24 +1,19 @@
 package classProperties;
 
-import static org.junit.Assert.assertTrue;
 
-import java.awt.AWTException;
-import java.awt.event.KeyEvent;
-
-import org.junit.Test; 
-import classProperties.KeyAvatarWithShieldProperties;
+//import classProperties.KeyAvatarWithShieldProperties;
 
 public class ElectionKeyAvatarWithShieldTest {
-	@Test
-	public void electionKeyAWS() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, AWTException{
-		KeyAvatarWithShieldProperties k= new KeyAvatarWithShieldProperties();
-	//	System.out.println("tecla arriba: "+k.getElectionKeyAvatarWithShield().getKeyUp());
-		
-	/*	ElectionKeyAvatarWithShield e= new ElectionKeyAvatarWithShield(k.getElectionKeyAvatarWithShield().getKeyUp(),k.getElectionKeyAvatarWithShield().getKeyDown(),
-				k.getElectionKeyAvatarWithShield().getKeyLeft(),k.getElectionKeyAvatarWithShield().getKeyRight(),k.getElectionKeyAvatarWithShield().getKeyShoot(),
-				k.getElectionKeyAvatarWithShield().getKeyPower1(),k.getElectionKeyAvatarWithShield().getKeyPower2());
-		//System.out.println("tecla arriba: "+k.getElectionKeyAvatarWithShield().getKeyUp());
-		assertTrue(KeyEvent.VK_O == k.getElectionKeyAvatarWithShield().getKeyUp());*/
-	}
-	
+//	@Test
+//	public void electionKeyAWS() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, AWTException{
+//		KeyAvatarWithShieldProperties k= new KeyAvatarWithShieldProperties();
+//	//	System.out.println("tecla arriba: "+k.getElectionKeyAvatarWithShield().getKeyUp());
+//		
+//	/*	ElectionKeyAvatarWithShield e= new ElectionKeyAvatarWithShield(k.getElectionKeyAvatarWithShield().getKeyUp(),k.getElectionKeyAvatarWithShield().getKeyDown(),
+//				k.getElectionKeyAvatarWithShield().getKeyLeft(),k.getElectionKeyAvatarWithShield().getKeyRight(),k.getElectionKeyAvatarWithShield().getKeyShoot(),
+//				k.getElectionKeyAvatarWithShield().getKeyPower1(),k.getElectionKeyAvatarWithShield().getKeyPower2());
+//		//System.out.println("tecla arriba: "+k.getElectionKeyAvatarWithShield().getKeyUp());
+//		assertTrue(KeyEvent.VK_O == k.getElectionKeyAvatarWithShield().getKeyUp());*/
+//	}
+//	
 }
