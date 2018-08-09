5cef299e95278937b15cd34d528fe93be91e9221
fjgalvan <fjgalvanx87@gmail.com>
2018-08-09 19:03:10 -0300

    turnos

--- avatares/tp.pp2.app/src/main/java/control/AvatarWithShieldController.java
+++ avatares/tp.pp2.app/src/main/java/control/AvatarWithShieldController.java
@@ -1,7 +1,10 @@
 package control;
 
-import java.awt.AWTException;  
+import java.awt.AWTException;
 import java.awt.Point;
+
+import main.Constants;
+import classProperties.KeyAvatarProperties;
 import object.GameObject;
 import object.Weapon;
 import tablero.Map;
@@ -9,59 +12,68 @@ import tablero.Map;
 public class AvatarWithShieldController extends GameObjectController{
 	Integer cantShootingRange=0;
 	ControlColissionShoot d;
+	KeyAvatarProperties k;
+	ControlPositionMovement c;
+	boolean control= false;
 	
-	public AvatarWithShieldController(GameObject avatarWithShield, Map map, Weapon arma) 
+	public AvatarWithShieldController(GameObject avatar, Map map, Weapon arma) 
 	{
-		super(avatarWithShield, map, arma);
+		super(avatar, map, arma);
 		d= new ControlColissionShoot(avatar, map, arma);
+		k= new KeyAvatarProperties();
+		k.loadPropertiesByRoute(Constants.ROUTE_PROPERTIES_AvatarWithShield);
+		c= new ControlPositionMovement(avatar, map, arma);
 	}
-
-	public void controlAvatar(Point positionAvatarWithShield, Integer keyPressedInNow, GameObject enemy) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, AWTException //Esto se usa cuando se juego continuamente
+	
+	public void controlAvatar(Point positionAvatar, Integer keyPressedInNow, GameObject enemy)throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, AWTException //Esto se usa cuando se juego continuamente
 	{
-	/*	KeyAvatarWithShieldProperties k= new KeyAvatarWithShieldProperties();
-		ControlPositionMovement c= new ControlPositionMovement(avatar, map, arma);
-		ControlTurn t= new ControlTurn(avatar, map , arma);
-		
-		*/
+
 		//keyPressedInNow = keyListener.getKeyPressed();
-	/*	if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyUp()){//KeyEvent.VK_UP){
-			c.avanzar(positionAvatarWithShield);
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyUp().getKeyValue())){
+			c.avanzar(positionAvatar);
 		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyDown()){
-			c.retroceder(positionAvatarWithShield);
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyDown().getKeyValue())){
+			c.retroceder(positionAvatar);
 		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyLeft()){
-			t.girarHaciaIzquierda();
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyLeft().getKeyValue())){
+			avatar.turnToLeft();
 		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyRight()){
-			t.girarHaciaDerecha();
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyRight().getKeyValue())){
+			avatar.turnToRight();
 		}
-		if(keyPressedInNow == KeyEvent.VK_ESCAPE){
-			salir();
-		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyShoot()){
+//		if(keyPressedInNow == KeyEvent.VK_ESCAPE){
+//			salir();
+//		}
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyShoot().getKeyValue())){
+			control= true;
 			cantShootingRange= d.disparar(enemy);
+			isDisparoRealizado= true;
 		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyPower1()){
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyPower1().getKeyValue())){
 			//activarPoder1();
 		}
-		if(keyPressedInNow == k.getElectionKeyAvatarWithShield().getKeyPower2()){
+		if(keyPressedInNow.equals(k.getElectionKeyAvatar().getKeyPower2().getKeyValue())){
 			//activarPoder2();
 		}
-*/
+
 	}
-	
 	private void salir() {
 	}
 	public Integer getCantShootingRange() {
 		return cantShootingRange;
 	}
 	public boolean isColission(){
-		boolean huboChoque= d.isColission(cantShootingRange);
-		return huboChoque;
+		if(control){
+			control= false;
+			return d.isColission(cantShootingRange);
+		}
+		else{
+			return false;
+		}
 	}
 
 	public ControlColissionShoot getD() {
 		return d;
 	}
+	
 }
